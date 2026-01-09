014523634739290b2e2b4658227893d4c2d97071
Lucas Camargo <nascimentocamargolucas@gmail.com>
2026-01-09 08:09:28 -0300

    Developing project.

--- tests/login/test_post_login.py
+++ tests/login/test_post_login.py
@@ -9,3 +9,27 @@ def test_login_success():
     assert body["message"] == "Login realizado com sucesso"
     assert "authorization" in body
     assert body["authorization"].startswith("Bearer ")
+    
+
+def test_login_email_invalido():
+    response = login(
+        email="email_invalido@qa.com",
+        password="teste"
+    )
+
+    body = response.json()
+
+    assert response.status_code == 401
+    assert body["message"] == "Email e/ou senha inválidos"
+
+
+def test_login_senha_invalida():
+    response = login(
+        email="fulano@qa.com",
+        password="senha_errada"
+    )
+
+    body = response.json()
+
+    assert response.status_code == 401
+    assert body["message"] == "Email e/ou senha inválidos"
