3bb1a16e531f127f6598288a84ea73d311351b0b
Lucas Camargo <nascimentocamargolucas@gmail.com>
2026-02-12 14:38:06 -0300

    Developing project.

---
+++ tests/login/test_login.py
@@ -0,0 +1,29 @@
+import requests
+
+class TestLogin:
+    url = "https://serverest.dev/login"
+
+    def test_login_com_sucesso_200(self, usuario_global):
+        """Valida login com credenciais válidas (Status 200)"""
+        payload = {
+            "email": usuario_global["email"],
+            "password": usuario_global["password"]
+        }
+        response = requests.post(self.url, json=payload)
+        
+        # Validações baseadas no screenshot
+        assert response.status_code == 200
+        assert response.json()["message"] == "Login realizado com sucesso"
+        assert "authorization" in response.json()
+
+    def test_login_email_senha_invalidos_401(self):
+        """Valida erro ao logar com credenciais inexistentes (Status 401)"""
+        payload = {
+            "email": "email_inexistente_@qa.com",
+            "password": "senha_errada"
+        }
+        response = requests.post(self.url, json=payload)
+        
+        # Validações baseadas no screenshot
+        assert response.status_code == 401
+        assert response.json()["message"] == "Email e/ou senha inválidos"
